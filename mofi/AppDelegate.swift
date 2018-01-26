009921851bd287ef2ef2ad3e8e1f1f7b3427e360
crocus7724 <account@crocus.jp>
2018-01-26 23:37:33 +0900

    Initial Commit

---
+++ mofi/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mofi
+//
+//  Created by 山本一樹 on 2018/01/26.
+//  Copyright © 2018 Kazuki Yamamoto. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mofi/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mofi
+//
+//  Created by 山本一樹 on 2018/01/26.
+//  Copyright © 2018 Kazuki Yamamoto. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
