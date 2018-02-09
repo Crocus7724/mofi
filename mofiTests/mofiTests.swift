f63dfc75841ea16ef7f518b5753b3581dcd5c2da
crocus7724 <account@crocus.jp>
2018-02-10 06:35:52 +0900

    [Update] failed test

--- mofiTests/mofiTests.swift
+++ mofiTests/mofiTests.swift
@@ -23,6 +23,7 @@ class mofiTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTFail()
     }
     
     func testPerformanceExample() {
